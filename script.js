efb47937da0d317d991111e8afc3a8b6a43b4d6e
awsxde <ali23khour@gmail.com>
2022-12-02 14:50:55 +0330

    few changes, fixed some bugs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //elements
 let btn = document.querySelectorAll(".number");
 let input = document.querySelector(".input");
+let zero = document.querySelector(".zero");
 
 //ops
 let div = document.querySelector(".div");
@@ -13,30 +14,31 @@ let pow = document.querySelector(".pow");
 let dot = document.querySelector(".dot");
 let equal = document.querySelector(".equal");
 let clear = document.querySelector(".clear");
+// let op = document.querySelector(".op");
 
-//to num btns can work
+//event listener for nums
 Array.from({ length: btn.length }, (_, i) => i).map((i) =>
   btn[i].addEventListener("click", () => (input.value += btn[i].textContent))
 );
 
 div.addEventListener("click", () => {
-  input.value += "/";
+  input.value += input.value === "" ? "" : div.textContent;
 });
 
 mul.addEventListener("click", () => {
-  input.value += "*";
+  input.value += input.value === "" ? "" : mul.textContent;
 });
 
 min.addEventListener("click", () => {
-  input.value += "-";
+  input.value += input.value === "" ? "" : min.textContent;
 });
 
 plus.addEventListener("click", () => {
-  input.value += "+";
+  input.value += input.value === "" ? "" : plus.textContent;
 });
 
 rem.addEventListener("click", () => {
-  input.value += "%";
+  input.value += input.value === "" ? "" : rem.textContent;
 });
 
 // sqrt.addEventListener("click", () => {
@@ -49,13 +51,25 @@ rem.addEventListener("click", () => {
 
 dot.addEventListener("click", () => {
   input.value +=
-    input.value === "" ? "0." : input.value.includes(".") ? "" : ".";
+    input.value === ""
+      ? "0" + dot.textContent
+      : input.value.includes(".")
+      ? ""
+      : dot.textContent;
 });
 
 equal.addEventListener("click", () => {
-  input.value = eval(input.value);
+  input.value = input.value === "" ? "" : eval(input.value);
 });
 
 clear.addEventListener("click", () => {
   input.value = "";
+});
+
+// op.addEventListener("click", () => {
+//   input.value += input.value === "" ? "" : op.textContent;
+// });
+
+zero.addEventListener("click", () => {
+  input.value += input.value === "" ? "" : zero.textContent;
 });

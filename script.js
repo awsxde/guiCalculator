5ae97f67b3a24bfffa51c65e32c62aa256839219
awsxde <ali23khour@gmail.com>
2022-12-04 18:01:40 +0330

    cleaned some codes and add features

--- script.js
+++ script.js
@@ -9,12 +9,9 @@ let mul = document.querySelector(".mul");
 let min = document.querySelector(".min");
 let plus = document.querySelector(".plus");
 let rem = document.querySelector(".rem ");
-let sqrt = document.querySelector(".sqrt");
-let pow = document.querySelector(".pow");
 let dot = document.querySelector(".dot");
 let equal = document.querySelector(".equal");
 let clear = document.querySelector(".clear");
-// let op = document.querySelector(".op");
 
 //event listener for nums
 Array.from({ length: btn.length }, (_, i) => i).map((i) =>
@@ -41,24 +38,13 @@ rem.addEventListener("click", () => {
   input.value += input.value === "" ? "" : rem.textContent;
 });
 
-// sqrt.addEventListener("click", () => {
-//   input.value = input.value ** 0.5;
-// });
-
-// pow.addEventListener("click", () => {
-//   input.value += " " + pow.textContent + " ";
-// });
-
 dot.addEventListener("click", () => {
-  input.value +=
-    input.value === ""
-      ? "0" + dot.textContent
-      : input.value.includes(".")
-      ? ""
-      : dot.textContent;
+  input.value += !input.value ? "0." : input.value.includes(".") ? "" : ".";
 });
 
 equal.addEventListener("click", () => {
+  // console.log([...input.value].map(Number).includes(NaN));
+  // [...input.value].map(Number).includes(NaN) ? "Error" :
   input.value = input.value === "" ? "" : eval(input.value);
 });
 
@@ -66,10 +52,35 @@ clear.addEventListener("click", () => {
   input.value = "";
 });
 
+zero.addEventListener("click", () => {
+  input.value += input.value === "" ? "" : zero.textContent;
+});
+
+// let op = document.querySelector(".op");
+
 // op.addEventListener("click", () => {
 //   input.value += input.value === "" ? "" : op.textContent;
 // });
 
-zero.addEventListener("click", () => {
-  input.value += input.value === "" ? "" : zero.textContent;
-});
+//ascii codes for
+// 0 = 48
+// 9 = 57
+
+// let preview = document.querySelector(".preview");
+
+// input.addEventListener("input", (e) => {
+//   let x = e.target.value;
+//   preview.innerHTML = !(47 < x.charCodeAt() && x.charCodeAt() < 58) ? x : "";
+// });
+
+// let sqrt = document.querySelector(".sqrt");
+
+// sqrt.addEventListener("click", () => {
+//   input.value = input.value ** 0.5;
+// });
+
+// let pow = document.querySelector(".pow");
+
+// pow.addEventListener("click", () => {
+//   input.value += " " + pow.textContent + " ";
+// });

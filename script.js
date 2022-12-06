a5a5a5c444e96aa2ee5cc1f093173e7672435562
awsxde <ali23khour@gmail.com>
2022-12-07 00:31:57 +0330

    improvements

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 //elements
-let btn = document.querySelectorAll(".number");
-let input = document.querySelector(".input");
 let zero = document.querySelector(".zero");
+let input = document.querySelector(".input");
+let btn = document.querySelectorAll(".number");
+let answer = document.querySelector(".answer");
 
 //ops
+let e = document.querySelector(".e");
+let pi = document.querySelector(".pi");
+let op = document.querySelector(".op");
+let ln = document.querySelector(".ln");
+let ex = document.querySelector(".ex");
 let div = document.querySelector(".div");
 let mul = document.querySelector(".mul");
 let min = document.querySelector(".min");
-let plus = document.querySelector(".plus");
-let rem = document.querySelector(".rem ");
 let dot = document.querySelector(".dot");
+let pow = document.querySelector(".pow");
+let rad = document.querySelector(".rad");
+let sin = document.querySelector(".sin");
+let cos = document.querySelector(".cos");
+let tan = document.querySelector(".tan");
+let log = document.querySelector(".log");
+let abs = document.querySelector(".abs");
+let rem = document.querySelector(".rem ");
+let plus = document.querySelector(".plus");
+let sqrt = document.querySelector(".sqrt");
+let pow2 = document.querySelector(".pow2");
+let sign = document.querySelector(".sign");
 let equal = document.querySelector(".equal");
 let clear = document.querySelector(".clear");
-let sqrt = document.querySelector(".sqrt");
-let pow = document.querySelector(".pow");
-let pow2 = document.querySelector(".pow2");
-let pi = document.querySelector(".pi");
-let e = document.querySelector(".e");
 let inverse = document.querySelector(".inverse");
+let flash = document.querySelector(".flash");
 
 //event listener for nums
 btn.forEach((i) =>
@@ -59,8 +71,23 @@ clear.addEventListener("click", () => {
 });
 
 zero.addEventListener("click", () => {
-  input.value += input.value === "" ? "" : zero.textContent;
+  input.value += input.value ? "0" : "";
 });
+
+let pushToHistory = () => {
+  input.value != "" && history.push(Number(input.value));
+  input.value = "";
+};
+
+let showHistory = () =>
+  (answer.textContent = history.reduce((a, b) => a + b, ""));
+
+let showAnswer = () => (answer.textContent = eval(answer.textContent));
+
+let equalArray = (str = "") => {
+  history.map((i) => (str += i));
+  history = [eval(str)];
+};
 
 let displayOps = document.querySelector(".displayOps");
 
@@ -68,7 +95,7 @@ let parentEng = document.querySelector(".parentEng");
 
 let count = 0;
 
-// parentEng.classList.remove("hide");
+parentEng.classList.remove("hide");
 
 displayOps.addEventListener("click", () => {
   count++;
@@ -99,6 +126,34 @@ e.addEventListener("click", () => {
 
 inverse.addEventListener("click", () => {
   input.value = 1 / input.value;
+});
+
+sin.addEventListener("click", () => {
+  input.value = Math.sin(input.value);
+});
+
+cos.addEventListener("click", () => {
+  input.value = Math.cos(input.value);
+});
+
+tan.addEventListener("click", () => {
+  input.value = Math.tan(input.value);
+});
+
+ln.addEventListener("click", () => {
+  input.value = Math.log(input.value);
+});
+
+log.addEventListener("click", () => {
+  input.value = Math.log(input.value) / Math.log(10);
+});
+
+ex.addEventListener("click", () => {
+  input.value = Math.E ** input.value;
+});
+
+abs.addEventListener("click", () => {
+  input.value = Math.abs(input.value);
 });
 
 // let op = document.querySelector(".op");

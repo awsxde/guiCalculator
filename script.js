41819eff6a63186258d0ac9532c01fa0d337cd51
awsxde <ali23khour@gmail.com>
2022-12-16 21:28:45 +0330

    fixed () button and some code cleanup

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let preview = document.querySelector(".preview");
 //ops
 let e = document.querySelector(".e");
 let pi = document.querySelector(".pi");
-let op = document.querySelectorAll(".op");
 let ln = document.querySelector(".ln");
 let ex = document.querySelector(".ex");
 let div = document.querySelector(".div");
@@ -21,6 +20,7 @@ let cos = document.querySelector(".cos");
 let tan = document.querySelector(".tan");
 let log = document.querySelector(".log");
 let abs = document.querySelector(".abs");
+let op = document.querySelectorAll(".op");
 let rem = document.querySelector(".rem ");
 let plus = document.querySelector(".plus");
 let sqrt = document.querySelector(".sqrt");
@@ -28,47 +28,30 @@ let pow2 = document.querySelector(".pow2");
 let sign = document.querySelector(".sign");
 let equal = document.querySelector(".equal");
 let clear = document.querySelector(".clear");
-// let flash = document.querySelector(".flash");
+let flash = document.querySelectorAll(".flash");
 let inverse = document.querySelector(".inverse");
 let backspace = document.querySelector(".backspace");
 let parentEng1 = document.querySelector(".parentEng1");
 let parentEng2 = document.querySelector(".parentEng2");
 let displayOps = document.querySelector(".displayOps");
+let parenthesis = document.querySelector(".parenthesis");
 
 //variables
 let count1 = 0;
-// let count2 = 0;
+let count2 = 0;
 
-let nums = input.value
-  .replace(/[%/*-+]/g, "$")
-  .split("$")
-  .filter((i) => i !== "")
-  .map(Number);
+//functions
+function inputValues() {
+  return input.value
+    .replace(/[/*-+]/g, "$")
+    .split("$")
+    .filter((i) => i !== "")
+    .map(Number);
+}
 
-let operators = input.value.replace(/\d/g, "").split("");
-
-// console.log(nums);
-// console.log(operators);
-
-// input.value = "2%+1*2";
-
-//bad way
-// operators = operators
-//   .map(function (i, index) {
-//     if (i === "%") nums[index] /= 100;
-//     return i;
-//   })
-//   .filter((i) => i !== "%");
-
-// console.log(nums[nums.length - 1]);
-
-// equal.addEventListener("click", () => assignToInput(input.value));
-
-equal.addEventListener("click", () => {
-  // preview.textContent = eval(input.value.replace(/%/g, "/100"));
-  input.value = eval(input.value);
-  preview.textContent = "";
-});
+function operators() {
+  return input.value.replace(/\d/g, "").split("");
+}
 
 // const dotHandler = (x = nums.length, y = operators.length) => {
 //   x === y ?
@@ -76,54 +59,78 @@ equal.addEventListener("click", () => {
 //   (input.value += !input.value ? "0." : input.value.includes(".") ? "" : ".");
 // };
 
-//functions
-btn.forEach((i) =>
-  i.addEventListener("click", () => {
-    input.value += i.textContent;
-    if ([..."*/-+"].some((i) => input.value.includes(i)))
-      preview.textContent = eval(input.value);
-  })
-);
+function btnHandler(i) {
+  input.value += i.textContent;
+  if ([..."*/-+"].some((i) => input.value.includes(i)))
+    preview.textContent = eval(input.value);
+}
 
-const addToInput = (value) => (input.value += !input.value ? "" : value);
+function addToInput(value) {
+  input.value += !input.value ? "" : value;
+}
 
-const assignToInput = (value) =>
-  (input.value = !input.value ? "" : eval(value));
+function assignToInput(value) {
+  input.value = !input.value ? "" : eval(value);
+}
 
-const clearFun = () => {
+function clearFun() {
   input.value = "";
   preview.textContent = "";
-};
+}
 
-const showExtraBtns = () =>
+function equalFun() {
+  input.value = eval(input.value);
+  preview.textContent = "";
+}
+
+function showExtraBtns() {
   ++count1 && count1 % 2
     ? parentEng1.classList.remove("hide")
     : parentEng1.classList.add("hide");
-
-// let count1 = 0;
-let count2 = 0;
-
-let flash = document.querySelectorAll(".flash");
+}
 
 function unhide(x, y) {
   x.classList.remove("hide");
   y.classList.add("hide");
 }
 
-flash.forEach((i) =>
-  i.addEventListener("click", () => {
-    ++count2 && count2 % 2
-      ? unhide(parentEng2, parentEng1)
-      : unhide(parentEng1, parentEng2);
-  })
-);
+function flashHandler() {
+  ++count2 && count2 % 2
+    ? unhide(parentEng2, parentEng1)
+    : unhide(parentEng1, parentEng2);
+}
 
-//fixing multi op problem
-const addToInputOp = (value) =>
-  (input.value +=
-    !input.value || [..."*/+-"].includes(input.value.slice(-1)[0])
-      ? ""
-      : value);
+function addToInputOp(value) {
+  input.value +=
+    !input.value || [..."*/+-"].includes(input.value.slice(-1)[0]) ? "" : value;
+}
+
+function pt() {
+  let count = input.value
+    .split("")
+    .filter((i) => ["(", ")"].includes(i))
+    .reduce((a, b) => a + (b === "(" ? 1 : -1), 0);
+  // input.value += !input.value ? "" : "*";
+  input.value += count <= 0 ? "(" : ")";
+}
+
+function clearPreview() {
+  preview.textContent = "";
+}
+
+function backspaceHandler() {
+  input.value = !input.value ? "" : input.value.slice(0, -1);
+  preview.textContent = eval(input.value.slice(0, -1));
+}
+
+clear.addEventListener("click", clearFun);
+equal.addEventListener("click", equalFun);
+parenthesis.addEventListener("click", pt);
+displayOps.addEventListener("click", showExtraBtns);
+backspace.addEventListener("click", backspaceHandler);
+op.forEach((i) => i.addEventListener("click", clearPreview));
+flash.forEach((i) => i.addEventListener("click", flashHandler));
+btn.forEach((i) => i.addEventListener("click", () => btnHandler(i)));
 
 //one event listener instead of billions
 let test = document.querySelectorAll(".test");
@@ -140,62 +147,39 @@ test.forEach((i) =>
 
 // plus.addEventListener("click", () => addToInput(plus.textContent));
 
-rem.addEventListener("click", () => assignToInput(input.value / 100));
+// rem.addEventListener("click", () => assignToInput(input.value / 100));
 // rem.addEventListener("click", () => assignToInput(rem.textContent));
 
 // dot.addEventListener("click", dotHandler);
-
-clear.addEventListener("click", clearFun);
 
 // zero.addEventListener("click", () => {
 //   input.value += input.value === "" ? "" : "0";
 // });
 
-displayOps.addEventListener("click", showExtraBtns);
+// sqrt.addEventListener("click", () => assignToInput(input.value ** 0.5));
 
-sqrt.addEventListener("click", () => assignToInput(input.value ** 0.5));
+// pow.addEventListener("click", () => addToInput("**"));
 
-pow.addEventListener("click", () => addToInput("**"));
+// pow2.addEventListener("click", () => addToInput("**2"));
 
-pow2.addEventListener("click", () => addToInput("**2"));
+// pi.addEventListener("click", () => addToInput(Math.PI));
 
-pi.addEventListener("click", () => addToInput(Math.PI));
+// e.addEventListener("click", () => addToInput(Math.E));
 
-e.addEventListener("click", () => addToInput(Math.E));
+// inverse.addEventListener("click", () => assignToInput(1 / input.value));
 
-inverse.addEventListener("click", () => assignToInput(1 / input.value));
+// sin.addEventListener("click", () => assignToInput(Math.sin(input.value)));
 
-sin.addEventListener("click", () => assignToInput(Math.sin(input.value)));
+// cos.addEventListener("click", () => assignToInput(Math.cos(input.value)));
 
-cos.addEventListener("click", () => assignToInput(Math.cos(input.value)));
+// tan.addEventListener("click", () => assignToInput(Math.tan(input.value)));
 
-tan.addEventListener("click", () => assignToInput(Math.tan(input.value)));
+// ln.addEventListener("click", () => assignToInput(Math.ln(input.value)));
 
-ln.addEventListener("click", () => assignToInput(Math.ln(input.value)));
+// log.addEventListener("click", () =>
+//   assignToInput(Math.log(input.value) / Math.log(10))
+// );
 
-log.addEventListener("click", () =>
-  assignToInput(Math.log(input.value) / Math.log(10))
-);
+// ex.addEventListener("click", () => assignToInput(Math.E ** input.value));
 
-ex.addEventListener("click", () => assignToInput(Math.E ** input.value));
-
-abs.addEventListener("click", () => assignToInput(Math.abs(input.value)));
-
-backspace.addEventListener("click", () => {
-  input.value = !input.value ? "" : input.value.slice(0, -1);
-});
-
-op.forEach((i) =>
-  i.addEventListener("click", () => {
-    preview.textContent = "";
-  })
-);
-
-let parenthesis = document.querySelector(".parenthesis");
-
-let count = 0;
-parenthesis.addEventListener("click", () => {
-  //state 1 : no parenthesis exist
-  count += 1;
-  input.value += count === 0 ? "" : count > 0 ? "(" : ")";
-});
+// abs.addEventListener("click", () => assignToInput(Math.abs(input.value)));
